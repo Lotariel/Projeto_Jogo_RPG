71b4ba24e39a63f37b3f30df5b4a3b9991c2a68c
Daniel Rocha <danihy7@gmail.com>
2020-09-12 16:08:57 -0300

    Inserido um botão na tela de login

--- Projeto_Jogo_RPG_WEB/Login.aspx.designer.cs
+++ Projeto_Jogo_RPG_WEB/Login.aspx.designer.cs
@@ -31,5 +31,14 @@ namespace Projeto_Jogo_RPG_WEB
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Controle btnLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
     }
 }
