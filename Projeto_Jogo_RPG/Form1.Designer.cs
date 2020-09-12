a8780a6a9c3df6b035d485173628d8f2129bbad0
Guilherme WilliamGui <gui-w-lima@hotmail.com>
2020-09-12 18:46:18 -0300

    TESTE GUI

--- Projeto_Jogo_RPG/Form1.Designer.cs
+++ Projeto_Jogo_RPG/Form1.Designer.cs
@@ -35,30 +35,29 @@
             // lbltitulo
             // 
             this.lbltitulo.AutoSize = true;
-            this.lbltitulo.Location = new System.Drawing.Point(442, 14);
-            this.lbltitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbltitulo.Location = new System.Drawing.Point(295, 9);
             this.lbltitulo.Name = "lbltitulo";
-            this.lbltitulo.Size = new System.Drawing.Size(169, 20);
+            this.lbltitulo.Size = new System.Drawing.Size(114, 13);
             this.lbltitulo.TabIndex = 0;
             this.lbltitulo.Text = "TELA DE CADASTRO";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(47, 95);
+            this.button1.Location = new System.Drawing.Point(332, 142);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(128, 31);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "HENRIQUE";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbltitulo);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
